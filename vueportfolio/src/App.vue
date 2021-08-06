6c95ae771c1989d228057feb235d34882fcf9448
santanu.phukan <santanu.phukan@metricstream.com>
2021-08-06 12:09:06 +0530

    vuetify

--- vueportfolio/src/App.vue
+++ vueportfolio/src/App.vue
@@ -1,5 +1,49 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer 
+      v-model="drawer"
+      app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
     <v-main>
       <router-view/>
     </v-main>
@@ -7,12 +51,14 @@
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-}
+  export default {
+    name: 'App',
+    data: () => ({ 
+      drawer: null, 
+      items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard', to:'/' },
+          { title: 'About', icon: 'mdi-help-box', to:'/about'  },
+        ],
+      }),
+  }
 </script>
